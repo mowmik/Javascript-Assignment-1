8c297d694ee4ce098db29c50fdf7942d17bd7d8d
Mowmik Saroar <mowmiksaroarmim0@gmail.com>
2024-05-31 18:26:23 +0600

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,103 @@
+  // 1 no answer
+let carName = "volvo"
+console.log(carName)
+
+  // 2 no answer
+const assign = `firstName = Mowmik lastName = Saroar age = 23`
+console.log(assign);
+
+  // 3 no answer
+let x = 10;
+let y = 5;
+let mult = x * y;
+console.log(mult);
+
+/* 4-10 no answers are given below
+
+  // 4 no answer
+let length = 16;
+console.log(typeof length);
+// number 
+let lastName = "Johnson"; // string 
+console.log(typeof lastName);
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // object
+console.log(typeof x); 
+
+
+  // 5 no answer
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+    <script>
+        function myFunction() {
+            alert("Hello World!");
+        }
+
+        myFunction();
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+  // 6 no answer
+const person = {
+    name: "John",
+    age: 50
+  };
+  console.log(person.name + " is " + person.age); 
+
+
+   // 7 no answer
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <button onclick="alert('Thank you for clicking me')">Click me</button>
+<script src="index.js"></script>
+</body>
+</html>
+
+
+  // 8 no answer
+const cars = ["Volvo", "Jeep", "Mercedes"];
+console.log(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+ Brand[0]="Ford"
+  console.log(Brand);
+
+
+  // 9 no answer
+let random = Math.random();
+console.log(random);
+
+let max = Math.max(10,20)
+console.log(max);
+
+let square = Math.sqrt(9);
+console.log(square);
+
+
+  // 10 no answer
+let x = 10;
+let y = 5;
+console.log(x > y); // This will alert "true"
+
+let age = 18;
+let message = age > 18 ? "Old enough" : "Too young";
+console.log(message);
+ 
+*/
